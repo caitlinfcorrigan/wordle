316658503bdd5f94c46e0e4db91292956001f165
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-10-06 14:23:05 -0700

    creates jest test file and creates comparison function:

--- main.js
+++ main.js
@@ -5,10 +5,20 @@
         inSamePos: "rgb(50, 155, 80)",
     }
 
+    // Mini dictionary for testing
+    const DICT  = {
+        a: ["apple", "atoms", "antsy"],
+        b: ["batty", "backs", "butts"],
+        c: ["catty", "chats", "comma"]
+    }
+
 /*----- STATE VARIABLES -----*/
+    let secretWord;
     let board;
-    // Initialize later as an array that 
-    let guess; 
+
+    // Initialize later as an array? 
+    let userGuess; 
+    let submitGuess = document.querySelector("#guess")
 
     // Count the number of guesses? Or just do a check of the board's array?
     let guessCount;
@@ -16,22 +26,81 @@
     // To return the outcome (win or loss)
     let outcome;
 
-/*----- CACHED ELEMENTS -----*/
+/*----- CACHED HTML ELEMENTS -----*/
     const outcomeMessage = document.querySelector("h2");
-    const playAgain = document.querySelector("button");
+    const playAgain = document.querySelector("#play-again");
 
     // Do I need to cache the gameboard?
 
 
 /*----- EVENT LISTENERS -----*/
-    // Listener for typed letters 
-    document.addEventListener("keydown")
+    // Listener for typed letters -- When user types, spell out the guess on screen
+    // document.addEventListener("keydown", spellGuess)
 
-    // Listener for Enter
+    // Listener for Enter button
 
+
+    // Temp listeners for Input & Guess buttons
+    submitGuess.addEventListener("click", compareGuess);
 
     // Listener for playAgain
     playAgain.addEventListener("click", init);
 
 
 /*----- FUNCTIONS -----*/
+
+// Pick a secret word & hold in secretWord
+
+
+// Test the user's guess
+userGuess = "lucky";
+secretWord = "cutie"
+function renderGuess(userGuess, secretWord) {
+    // For each letter in user guess, check secretWord
+    Array.from(userGuess).forEach((char,idx) => {
+        if(char === secretWord[idx]) {
+            console.log("exact match");
+            return "inSamePos";
+        }
+        else if (secretWord.includes(char)){   
+            console.log("it's there somewhere");
+            return "inDiffPos";
+        }
+        else{
+            console.log("no dice");
+            return "notIn";
+        }
+        // console.log(char);
+        // console.log(secretWord[idx])
+    })
+        // 
+}
+renderGuess(userGuess, secretWord)
+
+
+function init() {
+    // Initialize empty board in JS
+    board = [
+        [], // Row 0
+        [], // Row 1
+        [], // Row 2
+        [], // Row 3
+        [], // Row 4
+        [], // Row 5
+    ]
+    // TBD - Figure out how to grab a secret word
+    secretWord = "apple";
+    outcome = null;
+    render()
+}
+
+function render() {
+    spellGuess(e);
+    playAgain.style.visibility = winner ? 'visible' : 'hidden';
+}
+
+function compareGuess() {
+
+}
+
+
